55858f25d7f14a68622d3fb939611526e166fb8b
Rais-K <raissaklls@gmail.com>
2022-12-03 21:16:32 -0300

    Gerando inimigos de acordo com a dificuldade do level (quanto mais alto maior  achance do inimigo 2)

--- Assets/Scripts/GeradorInimigos.cs
+++ Assets/Scripts/GeradorInimigos.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 
 public class GeradorInimigos : MonoBehaviour
@@ -7,7 +8,7 @@ public class GeradorInimigos : MonoBehaviour
     [SerializeField] private GameObject[] inimigos;
 
     private int pontos = 0;
-    private int level = 1;
+    [SerializeField] private int level = 1;
 
     private float esperaInimigo = 0f;
     [SerializeField] float tempoEspera = 5f;
@@ -33,9 +34,23 @@ public class GeradorInimigos : MonoBehaviour
         if (esperaInimigo < 0f)
         {
 
+            GameObject inimigoCriado;
+            
+
+            //Decidindo qual inimigo vai ser criado com base no level
+            float chance = Random.Range(0f, level);
+            if (chance > 2f)
+            {
+                inimigoCriado = inimigos[1];
+            }
+            else
+            {
+                inimigoCriado = inimigos[0];
+            }
+
             //criando um inimigo
             Vector3 posicao = new Vector3(Random.Range(-8f, 8f), Random.Range(6f, 16f), 0f);
-            Instantiate(inimigos[0], posicao, transform.rotation);
+            Instantiate(inimigoCriado, posicao, transform.rotation);
 
             //reiniciando a espera
             esperaInimigo = tempoEspera;
